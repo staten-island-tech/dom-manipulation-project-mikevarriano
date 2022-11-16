9f4d25a86cf0dc1fd83bf9e1c0e34d42ae2e82ef
mikevarriano <113132199+mikevarriano@users.noreply.github.com>
2022-11-16 14:01:25 -0500

    wwwwww

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const DOMselectors = {
   input: document.querySelector("#input"),
 };
 const cat = "TELLER";
-DOMselectors.box.insertAdjacentHTML(
-  "beforebegin",
-  `<h1>FORTUNE ${cat}</h1>`
-);
+DOMselectors.box.insertAdjacentHTML("beforebegin", `<h1>FORTUNE ${cat}</h1>`);
+const rchoices = [
+  "you will marry Andrew Tate",
+  "what color is your Bugatti???",
+  "you will have thirty children",
+  "you are destined for success",
+];
+function RandomChoice() {
+  let rand = Math.random();
+  let choice = Math.floor(rand * rchoices.length);
+  console.log(rand, rchoices.length);
+  return rchoices[choice];
+}
+const mike = "mike";
 DOMselectors.button.addEventListener("click", function () {
   let input = DOMselectors.input.value;
-  DOMselectors.box.insertAdjacentHTML("beforeend", `<p>${input}, You Are Destined For Success</p>`);
-DOMselectors.input.value = "";
+  DOMselectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p>${input}, ${RandomChoice()}</p>`
+  );
+  DOMselectors.input.value = "";
 });
-DOMselectors.clearbtn.addEventListener("click", function (){
- let input = DOMselectors.input.value;
- DOMselectors.input.value = "";
-
+DOMselectors.clearbtn.addEventListener("click", function () {
+  let input = DOMselectors.input.value;
+  DOMselectors.input.value = "";
 });
